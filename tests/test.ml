51fa7256404f6ea6b5ec54b30ed277cbb0561073
arty <art.yerkes@gmail.com>
2021-06-26 10:51:54 -0700

    First failing test

--- tests/test.ml
+++ tests/test.ml
@@ -1,2 +1,46 @@
 open Compiler
 
+type test_spec =
+  { expected : string compileResult
+  ; opts : compilerOpts
+  ; input : string
+  }
+
+let emptyOpts =
+  { includeDirs = []
+  ; filename = "test.clvm"
+  ; assemble = true
+  ; readNewFile = fun _ name _ ->
+      Compiler.CompileError (name, Srcloc.start, "include unimplemented")
+  }
+
+let emptyCompile = { emptyOpts with assemble = true }
+
+let tests =
+  [ { expected = CompileOk "00"
+    ; opts = emptyOpts
+    ; input = "()"
+    }
+  ]
+
+let _ =
+  let failed = ref false in
+  let _ =
+    tests
+    |> List.iteri
+      (fun i e ->
+         let cr = compile_file e.opts e.input in
+         if cr <> e.expected then
+           begin
+             failed := true ;
+             Js.log "\nunexpected output:" ;
+             Js.log cr ;
+             Js.log "for input:" ;
+             Js.log e.input ;
+             Node.Process.process##abort ()
+           end
+         else
+           Js.log ("\r" ^ (string_of_int i) ^ "\r")
+      )
+  in
+  Js.log "\nTests PASSED\n"
